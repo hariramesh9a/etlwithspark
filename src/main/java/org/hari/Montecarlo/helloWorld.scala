fda8513c0983a5f623ffdb847ae799a6a871f4df
hariramesh9a <akbank05>
2020-06-27 00:35:58 -0500

    hello

---
+++ src/main/java/org/hari/Montecarlo/helloWorld.scala
@@ -0,0 +1,36 @@
+package org.hari.Montecarlo
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.hive.HiveContext
+
+object helloWorld {
+
+  def main(args: Array[String]): Unit = {
+
+
+    val sc = new SparkConf().setAppName("MonteCarlo").setMaster("local[*]")
+    val sparkCont = new SparkContext(sc)
+    val sqlCont = new HiveContext(sparkCont)
+
+    val myList=Seq(1, 2, 3)
+
+
+    val data = sparkCont.parallelize(myList)
+
+    val data2= data
+
+    data2.foreach(x=>println(x))
+
+
+    println(data.count())
+
+    import sqlCont.implicits._
+
+
+    val df=data.toDF("col1")
+
+    df.show()
+
+  }
+
+}
